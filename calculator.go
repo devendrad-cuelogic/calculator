beab70868c6a156093aff86da90a2017f3c07413
devendradhanal <devendradhanal@gmail.com>
2017-04-10 08:48:46 +0530

    Initial commit

---
+++ calculator.go
@@ -0,0 +1,33 @@
+/*
+Package calculator provides with basic arithmatic operations
+like summation, substraction, multiplication and division.
+*/
+package calculator
+
+/*
+Sum method adds two numbers.
+*/
+func Sum(a int, b int) int {
+	return a + b
+}
+
+/*
+Sub method substracts two numbers.
+*/
+func Sub(a int, b int) int {
+	return a - b
+}
+
+/*
+Multiply method multiplies two numbers.
+*/
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+/*
+Divide method divides two numbers.
+*/
+func Divide(a int, b int) int {
+	return a / b
+}
